705b9c32b585c792776cb165918033bfcddfd21f
Christian Amani <alvesamani@gmail.com>
2017-04-29 18:41:09 +0000

    First Commit

---
+++ src/main/java/org/akanza/OurOsmsApplication.java
@@ -0,0 +1,12 @@
+package org.akanza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OurOsmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OurOsmsApplication.class, args);
+	}
+}
