e3634ef228f13fce6ae2eb95598cb59f31c63831
Christian Amani <alvesamani@gmail.com>
2017-07-08 16:24:06 +0000

    Feature : started implementation of wizard

---
+++ webui/lib/src/wizard/wizard_admin/wizard_admin_component.dart
@@ -0,0 +1,11 @@
+import 'package:angular2/angular2.dart';
+import 'package:angular_components/angular_components.dart';
+
+@Component(selector: "form-wizard-admin"
+  ,templateUrl: "wizard_admin_component.html"
+  ,styleUrls: const ["wizard_admin_component.css"]
+  ,directives: const [materialDirectives,COMMON_DIRECTIVES]
+  ,providers: const [materialProviders])
+class WizardAdminComponent {
+
+}
